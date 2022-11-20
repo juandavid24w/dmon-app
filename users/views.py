a16f007ef9dad8eacc4a1d75f7cd14d38037834d
Juan0402 <davidbare0402@gmail.com>
2022-11-20 14:52:39 -0600

    Removed get_context_data methods from the Student and Teacher signup views and replaced them with a class attribute extra_context

--- users/views.py
+++ users/views.py
@@ -1,34 +1,23 @@
 """Accounts view."""
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views import generic
 
 from users.forms import StudentSignUpForm, TeacherSignUpForm
-from users.models import CustomUser
 
 
-class StudentSignUpView(CreateView):
+class StudentSignUpView(generic.CreateView):
     """Sign up view for Student Users."""
 
-    model = CustomUser
     form_class = StudentSignUpForm
     template_name = "registration/signup_form.html"
     success_url = reverse_lazy("login")
-
-    def get_context_data(self, **kwargs):
-        """Set the `user_type` as "student" in the form data."""
-        kwargs["user_type"] = "student"
-        return super().get_context_data(**kwargs)
+    extra_context = {"user_type": "Student"}
 
 
-class TeacherSignUpView(CreateView):
+class TeacherSignUpView(generic.CreateView):
     """Sign up view for Teacher Users."""
 
-    model = CustomUser
     form_class = TeacherSignUpForm
     template_name = "registration/signup_form.html"
     success_url = reverse_lazy("login")
-
-    def get_context_data(self, **kwargs):
-        """Set the `user_type` as "student" in the form data."""
-        kwargs["user_type"] = "teacher"
-        return super().get_context_data(**kwargs)
+    extra_context = {"user_type": "Teacher"}
