7e6165218d6d9d258123c1847fc79d3b81cc1ffe
Juan0402 <davidbare0402@gmail.com>
2022-11-20 11:09:46 -0600

    Getting rid of about and team page from the polls app

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,11 +18,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("team", TemplateView.as_view(template_name="team.html"), name="team"),
-    path("about", TemplateView.as_view(template_name="about.html"), name="about"),
     path("", include("polls.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("users.urls")),
