d5aab3975354b2e3dd664994453c2e0657fe209c
Juan0402 <davidbare0402@gmail.com>
2022-11-21 09:32:51 -0600

    Renmaing customuser_form to generic_create_update_form

--- users/views.py
+++ users/views.py
@@ -11,6 +11,7 @@ class StudentSignUpView(generic.CreateView):
 
     model = CustomUser
     form_class = StudentSignUpForm
+    template_name = "generic_create_update_form.html"
     success_url = reverse_lazy("login")
     extra_context = {"title_text": "Student Sign Up", "button_text": "Register"}
 
@@ -20,5 +21,6 @@ class TeacherSignUpView(generic.CreateView):
 
     model = CustomUser
     form_class = TeacherSignUpForm
+    template_name = "generic_create_update_form.html"
     success_url = reverse_lazy("login")
     extra_context = {"title_text": "Teacher Sign Up", "button_text": "Register"}
