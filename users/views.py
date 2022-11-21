dc86ed518db7d219c68ebc6f4b00670083ece5f1
Juan0402 <davidbare0402@gmail.com>
2022-11-20 21:56:25 -0600

    Renamed the signup_form to customuser_form and using the attribute model in the signup views to automatically pick up the template

--- users/views.py
+++ users/views.py
@@ -3,13 +3,14 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 from users.forms import StudentSignUpForm, TeacherSignUpForm
+from users.models import CustomUser
 
 
 class StudentSignUpView(generic.CreateView):
     """Sign up view for Student Users."""
 
+    model = CustomUser
     form_class = StudentSignUpForm
-    template_name = "registration/signup_form.html"
     success_url = reverse_lazy("login")
     extra_context = {"user_type": "Student"}
 
@@ -17,7 +18,7 @@ class StudentSignUpView(generic.CreateView):
 class TeacherSignUpView(generic.CreateView):
     """Sign up view for Teacher Users."""
 
+    model = CustomUser
     form_class = TeacherSignUpForm
-    template_name = "registration/signup_form.html"
     success_url = reverse_lazy("login")
     extra_context = {"user_type": "Teacher"}
