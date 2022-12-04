767327bd22ac2ce1ea4ba5c249b38d4bb4de38ce
Juan0402 <davidbare0402@gmail.com>
2022-12-04 11:44:47 -0600

    Removing the plugins from the cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,10 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config);
+    e2e: {
+        setupNodeEvents(on, config) {},
+        baseUrl: 'http://127.0.0.1:8000/',
+        viewportHeight: 768,
+        viewportWidth: 1024,
     },
-    baseUrl: 'http://127.0.0.1:8000/',
-    viewportHeight: 768,
-    viewportWidth: 1024,
-  },
 });
