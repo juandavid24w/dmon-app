f6b32dcf518066f015f410c9f6554776f8d3b1a2
Juan0402 <davidbare0402@gmail.com>
2022-12-04 11:45:41 -0600

    Adding cypress command for login and register, removing wait()

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,7 +10,26 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
+Cypress.Commands.add('login', (email, password) => {
+    cy.visit('/accounts/login');
+    cy.get('input[name = "username"]').type(email);
+    cy.get('input[name = "password"]').type(password);
+    cy.get('form').submit();
+});
+
+Cypress.Commands.add('register', (url, first_name, last_name, email, password) => {
+    cy.visit(url);
+    cy.get('form');
+    cy.get('input[name = "first_name"]').type(first_name);
+    cy.get('input[name = "last_name"]').type(last_name);
+    cy.get('input[name = "email"]').type(email);
+    cy.get('input[name = "password1"]').type(password);
+    cy.get('input[name = "password2"]').type(password);
+    cy.contains('Register').click();
+    //after clicking should redirect to homepage
+    cy.url().should('include', '/');
+});
+
 //
 //
 // -- This is a child command --
