d477743118d13e4cf2310ced618483a38700a5ef
Juan0402 <davidbare0402@gmail.com>
2023-10-20 14:14:11 -0500

    Moving the signals to the UserProfile model file

--- user_profile/views.py
+++ user_profile/views.py
@@ -39,9 +39,25 @@ class UserProfileUpdateView(LoginRequiredMixin, UpdateView):
         """Owner of the object should be the current user."""
         return self.model.objects.filter(custom_user=self.request.user).first()
 
+    def get_context_data(self, **kwargs):
+        """Set the current value of first_name and last_name in the form from the current user."""
+        context = super().get_context_data(**kwargs)
+        context["form"] = UserProfileUpdateForm(
+            instance=self.request.user.userprofile,
+            initial={
+                "first_name": self.request.user.first_name,
+                "last_name": self.request.user.last_name,
+            },
+        )
+        return context
+
     def form_valid(self, form: object):
         """Set custom_user Field of the current object as the current user."""
         profile = form.save(commit=False)
         profile.custom_user = self.request.user
+        user = self.request.user
+        user.last_name = form.cleaned_data["last_name"]
+        user.first_name = form.cleaned_data["first_name"]
         profile.save()
+        user.save()
         return super().form_valid(form)
