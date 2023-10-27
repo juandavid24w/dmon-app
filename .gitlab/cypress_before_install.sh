6cfacd77cd0ec5710ce6224cd603a8af05cee147
Juan0402 <davidbare0402@gmail.com>
2023-10-27 14:06:08 -0500

    Upgrading pip before using the pip3 command in cypress before install script

--- .gitlab/cypress_before_install.sh
+++ .gitlab/cypress_before_install.sh
@@ -3,8 +3,8 @@ npm install cypress >/dev/null
 apt-get update >/dev/null
 apt-get install -y python3 python3-pip >/dev/null
 apt-get install -y libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb >/dev/null
-pip3 install -q virtualenv
 pip3 install --upgrade -q pip
+pip3 install -q virtualenv
 pip3 install --no-cache-dir -qr requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
