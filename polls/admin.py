e151e9494229a6d7e531a17d98e4a72216751f43
Juan0402 <davidbare0402@gmail.com>
2023-11-17 15:32:22 -0600

    Fixing imports

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from polls.models import Choice, Question
+from . import models
 
-admin.site.register(Question)
-admin.site.register(Choice)
+admin.site.register(models.Question)
+admin.site.register(models.Choice)
