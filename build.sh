67871ef70621e141868ac4cdb631123e2fcc5a08
Juan0402 <davidbare0402@gmail.com>
2024-03-14 19:48:25 -0500

    Moved the postgres pip requirement to a separate package

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 set -o errexit
 
 pip install -r requirements.txt
+pip install -r deploy-requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
